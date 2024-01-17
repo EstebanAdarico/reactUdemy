234b6e896e73e140a422e727b5be7cbbe0c4436f
bann_perseverance <estebangpa41@gmail.com>
2024-01-17 00:40:15 -0500

    prueba 11

--- practicando/tests/base-pruebas/09-promesas.test.js
+++ practicando/tests/base-pruebas/09-promesas.test.js
@@ -2,6 +2,7 @@ import { describe, test, expect } from "@jest/globals";
 import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
 
 describe("prueba de promesas 09-promesas", () => {
+   //done es para esperar hasta que el codigo termine
    test("getHeroeByIdAsync debe de retornar un heroe", (done) => {
       //todo se ejecutara en secuenciaz
       const id = 1;
@@ -9,6 +10,7 @@ describe("prueba de promesas 09-promesas", () => {
          //como es una tarea asincrona  se puede poner .then
          .then((hero) => {
             expect(hero).toEqual({
+               //aqui estoy ingresando lo que espero recibir 
                id: 1,
                name: "Batman",
                owner: "DC",
@@ -16,6 +18,7 @@ describe("prueba de promesas 09-promesas", () => {
             done();
          });
    });
+   //evaluando la execcion 
    test("getHeroeByIdAsync debe de obtener un error si no heroe existe", () => {
       //aumentando el tiempo
 
@@ -24,9 +27,10 @@ describe("prueba de promesas 09-promesas", () => {
       getHeroeByIdAsync(id)
          //como es una tarea asincrona  se puede poner .then
          .catch((error) => {
-
+            //
             expect(error).toBe("No se pudo encotrar el hero con id 100");
             done();
          });
    });
 });
+ 
